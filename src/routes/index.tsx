5636f7fbf6383ab300f85e243f3efd2e1c05abff
abdalaheh51-ux <2.39182003e+08+abdalaheh51-ux@users.noreply.github.com>
2026-07-29 13:59:44 +0000

    Update Instagram link to https://www.instagram.com/web__area/

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,7 +33,7 @@ const links: LinkItem[] = [
   {
     label: "انستجرام",
     sublabel: "شاهد أعمالنا ومشاريعنا المميزة",
-    href: "https://www.instagram.com/web_erea.2/",
+    href: "https://www.instagram.com/web__area/",
     icon: <Instagram className="h-6 w-6" strokeWidth={2.2} />,
     accent: "from-blue-600/20 to-sky-500/20",
   },
